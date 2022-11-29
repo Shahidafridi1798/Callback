215c493ba845e5210d5393601f1e357988cd4dbd
Shahidafridi1798 <princeafridi47@gmail.com>
2022-11-29 15:20:36 +0530

    callback

---
+++ script.js
@@ -0,0 +1,33 @@
+let h1=document.querySelector(".h1");
+let count=10;
+h1.innerText=count--;
+setTimeout(()=>{
+    h1.innerText=count--;
+    setTimeout(()=>{
+        h1.innerText=count--;
+        setTimeout(()=>{
+            h1.innerText=count--;
+            setTimeout(()=>{
+                h1.innerText=count--;
+                setTimeout(()=>{
+                    h1.innerText=count--;
+                    setTimeout(()=>{
+                        h1.innerText=count--;
+                        setTimeout(()=>{
+                            h1.innerText=count--;
+                            setTimeout(()=>{
+                                h1.innerText=count--;
+                                setTimeout(()=>{
+                                  h1.innerText=count--;
+                                  setTimeout(()=>{
+                                    h1.innerText="HAPPY INDEPENDENCE DAY!";
+                                  },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
